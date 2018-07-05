d40fb82f865d22128bedddc6faa7c9a53de15bf8
Abdul Hameed <raza2022@gmail.com>
2018-07-05 13:42:48 +0500

    heroku test

--- server.js
+++ server.js
@@ -7,18 +7,20 @@ import path from 'path'
 import Issue from './models/Issue';
 import User from './models/User';
 
-// var config = require('./config');
+import config from './config';
 
 const app = express();
 const router = express.Router();
 
 let baseUrl = 'localhost:27017';
+let dbURI = process.env.MONGODB_URI || `mongodb://localhost:27017/issues`;
+console.log(dbURI);
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect(`mongodb://localhost:27017/issues`);
+mongoose.connect(dbURI);
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully!');
@@ -106,35 +108,35 @@ app.use('/', router);
 app.listen(4000, () => console.log(`Express server running on port 4000`));
 
 
-router.post('/register', function(req, res) {
+// router.post('/register', function(req, res) {
+//
+//     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+//
+//     User.create({
+//             name : req.body.name,
+//             email : req.body.email,
+//             password : hashedPassword
+//         },
+//         function (err, user) {
+//             if (err) return res.status(500).send("There was a problem registering the user.")
+//             // create a token
+//             var token = jwt.sign({ id: user._id }, config.secret, {
+//                 expiresIn: 86400 // expires in 24 hours
+//             });
+//             res.status(200).send({ auth: true, token: token });
+//         });
+// });
 
-    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-
-    User.create({
-            name : req.body.name,
-            email : req.body.email,
-            password : hashedPassword
-        },
-        function (err, user) {
-            if (err) return res.status(500).send("There was a problem registering the user.")
-            // create a token
-            var token = jwt.sign({ id: user._id }, config.secret, {
-                expiresIn: 86400 // expires in 24 hours
-            });
-            res.status(200).send({ auth: true, token: token });
-        });
-});
-
-router.get('/me', function(req, res) {
-    var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-
-    jwt.verify(token, config.secret, function(err, decoded) {
-        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-
-        res.status(200).send(decoded);
-    });
-});
+// router.get('/me', function(req, res) {
+//     var token = req.headers['x-access-token'];
+//     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+//
+//     jwt.verify(token, config.secret, function(err, decoded) {
+//         if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+//
+//         res.status(200).send(decoded);
+//     });
+// });
 
 // GET /logout
 router.get('/logout', function(req, res, next) {
